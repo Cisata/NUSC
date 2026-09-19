dbd1da40bf738dbf23f163368e28dc999996185a
Cisata <99173675+Cisata@users.noreply.github.com>
2026-09-19 12:03:13 +0530

    Updated social links

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,6 @@
 const link=[
-    { name: "instagram", url: "https://www.instagram.com/nagalandunitedfc/" },
-    { name: "facebook", url: "https://www.facebook.com/NagalandUnitedFC" },
-    { name: "X", url: "https://twitter.com/NagalandUnited" },
+    { name: "instagram", url: "https://www.instagram.com/nagalandunited/" },
+    { name: "facebook", url: "https://www.facebook.com/p/Nagaland-United-Sports-Club-61571464628544/" },
+    { name: "X", url: "https://x.com/nusc2024/" },
 ]
 export default link;
